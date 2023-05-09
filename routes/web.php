f297bce7515e58a879e31705ed8d6f480800b21d
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-05-09 17:16:56 +0200

    Creazione e avviamento primo profetto in laravel

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    $welcome_message = "Benvenuti in Laravel";
+
+    $data = [
+
+        'welcome_message' => $welcome_message,
+        'Linguaggi' => [
+            'Html',
+            'CSS',
+            'JAVASCRIPT',
+            'PHP'
+        ],
+        'students' => [
+            'Classe 87'
+        ]
+    ];
+
+    return view('welcome', $data);
+
+
     return view('welcome');
-});
+})->name('home');
+
+
+Route::get('chi-siamo', function () {
+
+    return view('about', ['message' => 'Siamo la classe 87 e siamo arrivati a studiare laravel']);
+
+})->name('about');
